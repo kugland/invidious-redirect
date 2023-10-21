e35b1c5b66bf419b18ea18e20e398922789aaefc
André Kugland <kugland@gmail.com>
2023-10-21 17:32:03 -0300

    v0.2.0

--- invidious-redirect.ts
+++ invidious-redirect.ts
@@ -3,61 +3,58 @@
 // @author      André Kugland
 // @description Redirects YouTube videos to an Invidious instance.
 // @namespace   invidious-redirect
-// @version     0.1
+// @version     0.2
 // @match       *://*.youtube.com/
 // @match       *://*.youtube.com/*
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
 
-(function () {
-    "use strict";
+"use strict";
 
-    function makeUrl(videoId: string): string {
-        // Here you should put the URL to your Invidious instance.
-        return new URL(`http://127.0.0.1:3000/watch?v=${videoId}`).href;
+function makeUrl(videoId: string): string {
+    // Here you should put the URL to your Invidious instance.
+    return `http://127.0.0.1:3000/watch?v=${videoId}`;
+}
+
+function getVideoId(href: string): string {
+    const url = new URL(href, window.location.href);
+    if (url.pathname === '/watch') {
+        return url.searchParams.get('v');
+    } else {
+        const videoId = url.pathname.match(/^\/shorts\/([a-zA-Z0-9_-]+)$/)?.[1];
+        if (videoId)
+            return videoId;
     }
+    throw new Error(`Unable to parse URL: ${href}`);
+}
 
-    function getVideoId(href: string): string {
-        const url = new URL(href, window.location.href);
-        if (url.pathname === '/watch') {
-            return url.searchParams.get('v');
-        } else {
-            const videoId = url.pathname.match(/^\/shorts\/([a-zA-Z0-9_-]+)$/)?.[1];
-            if (videoId)
-                return videoId[1];
-        }
-        throw new Error(`Unable to parse URL: ${href}`);
+// Redirect on click.
+document.addEventListener('click', (event) => {
+    if (event.target instanceof HTMLElement) {
+        try {
+            const href = event.target.closest('a')?.getAttribute('href');
+            if (href) {
+                event.preventDefault();
+                event.stopPropagation();
+                window.location.assign(makeUrl(getVideoId(href)));
+            }
+        } catch (e) { }
     }
+}, true);
 
-    // Redirect on click.
-    document.addEventListener('click', (event) => {
-        if (event.target instanceof HTMLElement) {
-            try {
-                const href = event.target.closest('a')?.getAttribute('href');
-                if (href) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    window.location.assign(makeUrl(getVideoId(href)));
-                }
-            } catch (e) { }
-        }
-    }, true);
+// Redirect on url change.
+let currentUrl = window.location.href;
+setInterval(() => {
+    if (window.location.href !== currentUrl) {
+        currentUrl = window.location.href;
+        try {
+            window.location.replace(makeUrl(getVideoId(currentUrl)));
+        } catch (e) { }
+    }
+}, 150);
 
-    // Redirect on url change.
-    let currentUrl = window.location.href;
-    setInterval(() => {
-        if (window.location.href !== currentUrl) {
-            currentUrl = window.location.href;
-            try {
-                window.location.replace(makeUrl(getVideoId(currentUrl)));
-            } catch (e) { }
-        }
-    }, 150);
-
-    // Redirect on page load.
-    try {
-        window.location.replace(makeUrl(getVideoId(currentUrl)));
-    } catch (e) { }
-
-}());
+// Redirect on page load.
+try {
+    window.location.replace(makeUrl(getVideoId(currentUrl)));
+} catch (e) { }
