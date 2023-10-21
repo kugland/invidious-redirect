71573240f81b80b2e6d1457c4ee2f7aebe322d44
André Kugland <kugland@gmail.com>
2023-10-20 21:17:15 -0300

    small changes

--- invidious-redirect.js
+++ invidious-redirect.js
@@ -16,28 +16,29 @@
         return new URL(`http://127.0.0.1:3000/watch?v=${videoId}`).href;
     }
     function getVideoId(href) {
+        var _a;
         const url = new URL(href, window.location.href);
         if (url.pathname === '/watch') {
             return url.searchParams.get('v');
         }
         else {
-            const match = url.pathname.match(/^\/shorts\/([a-zA-Z0-9_-]+)$/);
-            if (match && match[1])
-                return match[1];
+            const videoId = (_a = url.pathname.match(/^\/shorts\/([a-zA-Z0-9_-]+)$/)) === null || _a === void 0 ? void 0 : _a[1];
+            if (videoId)
+                return videoId[1];
         }
         throw new Error(`Unable to parse URL: ${href}`);
     }
     // Redirect on click.
     document.addEventListener('click', (event) => {
+        var _a;
         if (event.target instanceof HTMLElement) {
-            const anchor = event.target.closest('a');
-            const href = anchor.getAttribute('href');
-            if (!href)
-                return;
             try {
-                event.preventDefault();
-                event.stopPropagation();
-                window.location.assign(makeUrl(getVideoId(href)));
+                const href = (_a = event.target.closest('a')) === null || _a === void 0 ? void 0 : _a.getAttribute('href');
+                if (href) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    window.location.assign(makeUrl(getVideoId(href)));
+                }
             }
             catch (e) { }
         }
