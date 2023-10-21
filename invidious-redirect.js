6e3e62f8e6bd45b9251c0604d29ef954610f0ea8
André Kugland <kugland@gmail.com>
2023-10-20 21:33:24 -0300

    target ES5 because why not

--- invidious-redirect.js
+++ invidious-redirect.js
@@ -13,27 +13,27 @@
     "use strict";
     function makeUrl(videoId) {
         // Here you should put the URL to your Invidious instance.
-        return new URL(`http://127.0.0.1:3000/watch?v=${videoId}`).href;
+        return new URL("http://127.0.0.1:3000/watch?v=".concat(videoId)).href;
     }
     function getVideoId(href) {
         var _a;
-        const url = new URL(href, window.location.href);
+        var url = new URL(href, window.location.href);
         if (url.pathname === '/watch') {
             return url.searchParams.get('v');
         }
         else {
-            const videoId = (_a = url.pathname.match(/^\/shorts\/([a-zA-Z0-9_-]+)$/)) === null || _a === void 0 ? void 0 : _a[1];
+            var videoId = (_a = url.pathname.match(/^\/shorts\/([a-zA-Z0-9_-]+)$/)) === null || _a === void 0 ? void 0 : _a[1];
             if (videoId)
                 return videoId[1];
         }
-        throw new Error(`Unable to parse URL: ${href}`);
+        throw new Error("Unable to parse URL: ".concat(href));
     }
     // Redirect on click.
-    document.addEventListener('click', (event) => {
+    document.addEventListener('click', function (event) {
         var _a;
         if (event.target instanceof HTMLElement) {
             try {
-                const href = (_a = event.target.closest('a')) === null || _a === void 0 ? void 0 : _a.getAttribute('href');
+                var href = (_a = event.target.closest('a')) === null || _a === void 0 ? void 0 : _a.getAttribute('href');
                 if (href) {
                     event.preventDefault();
                     event.stopPropagation();
@@ -44,8 +44,8 @@
         }
     }, true);
     // Redirect on url change.
-    let currentUrl = window.location.href;
-    setInterval(() => {
+    var currentUrl = window.location.href;
+    setInterval(function () {
         if (window.location.href !== currentUrl) {
             currentUrl = window.location.href;
             try {
