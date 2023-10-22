0ef200b9503f3525461bfaaa0fc4602c204ade09
André Kugland <kugland@gmail.com>
2023-10-21 22:01:21 -0300

    v0.2.2

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Invidious Redirect
 
-This userscript redirects YouTube videos to an Invidious instance. It was created by André Kugland.
+This userscript redirects YouTube videos to an Invidious instance.
 
 ## Usage
 
-To use this script, you'll need a userscript manager like Tampermonkey or Greasemonkey. Once the script is installed, it will automatically redirect YouTube videos to your Invidious instance when you click on a YouTube link. By default, the script is configured to redirect to `http://127.0.0.1:3000`, but you can change this by editing the `makeUrl` function in the script.
+To use this script, you'll need a userscript manager like Tampermonkey or Greasemonkey.
+You can install it from its page at [GreasyFork](https://greasyfork.org/en/scripts/477967-redirect-to-invidious),
+or by copying the contents of [invidious-redirect.js](invidious-redirect.js) into a new userscript.
+
+Once the script is installed, it will automatically redirect YouTube videos to your Invidious
+instance when you click on a YouTube link. By default, the script is configured to redirect to
+`http://127.0.0.1:3000`, but you can change this by editing the `makeUrl` function in the script.
 
 ## License
 
--- invidious-redirect.js
+++ invidious-redirect.js
@@ -4,16 +4,21 @@
 // @description Redirects YouTube videos to an Invidious instance.
 // @namespace   https://github.com/kugland
 // @license     MIT
-// @version     0.2.1
+// @version     0.2.2
 // @match       *://*.youtube.com/
 // @match       *://*.youtube.com/*
-// @grant       none
 // @run-at      document-start
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAAGFBMVEUhAABkZmN1d3ROyPFaXFm0trPR19WJi4ibscP/AAAAAXRSTlMAQObYZgAAALtJREFUeAFiwA4AZVG3FsIwDEzb6ax01vSs9J3qnfhFv8Dvoycd1Zd6Z3X5szzP2h++LYlxmb94UJGgGSuPlDPsWoQdvVH8GsAkkN+HKgkLAxWMfGoW1KOBDzwgMMZeqD8n8wpywA9yXRGT0iEsvaWGyOFaQ7Cm8ycgyFu40H0IQYPeyIYIqmlLajpIqwcVKuHCfMnK5CRBxtqcNaRBLNq3RBrkjAFlBq/EY/yM0BmyswYsCnzsrtJdtosnoyCIGirhmt8AAAAASUVORK5CYII=
 // ==/UserScript==
 "use strict";
+const instanceKey = 'invidious-redirect-instance';
+const defaultInstance = 'https://yewtu.be';
+// Get the Invidious instance URL from the user.
+localStorage.getItem(instanceKey) || localStorage.setItem(instanceKey, defaultInstance);
 function makeUrl(videoId) {
-    // Here you should put the URL to your Invidious instance.
-    return `http://127.0.0.1:3000/watch?v=${videoId}`;
+    const instance = localStorage.getItem(instanceKey);
+    // Let's construct a URL object to make sure it's valid.
+    return new URL(`${instance}/watch?v=${videoId}`).href;
 }
 function getVideoId(href) {
     var _a;
@@ -59,3 +64,48 @@ try {
     window.location.replace(makeUrl(getVideoId(currentUrl)));
 }
 catch (e) { }
+// Add a settings button.
+((fn) => {
+    if (document.readyState !== 'loading')
+        fn();
+    else
+        document.addEventListener('DOMContentLoaded', fn);
+})(() => {
+    const button = document.createElement('button');
+    button.id = 'set-invidious-url';
+    button.tabIndex = -1;
+    button.textContent = 'set invidious url';
+    button.addEventListener('click', () => {
+        let instance = prompt('Enter the URL of the Invidious instance to use:', localStorage.getItem(instanceKey) || defaultInstance);
+        if (instance) {
+            instance = instance.trim();
+            if (instance.endsWith('/'))
+                instance = instance.slice(0, -1);
+            localStorage.setItem(instanceKey, instance);
+        }
+        else {
+            localStorage.removeItem(instanceKey);
+        }
+    });
+    const css = document.createElement('style');
+    css.textContent = `
+        button#set-invidious-url {
+            font-size: 1.2em;
+            cursor: pointer;
+            position: fixed;
+            padding: 0.5em;
+            bottom: 0;
+            right: 0;
+            opacity: 0.25;
+            z-index: 99999;
+            font-family: inherit;
+            border: 0;
+        }
+        button#set-invidious-url:hover,
+        button#set-invidious-url:focus {
+            opacity: 1;
+        }
+    `;
+    document.body.appendChild(button);
+    document.head.appendChild(css);
+});
